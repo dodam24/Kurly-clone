6f275b93a8bba0e9cb9fa027d81454b451837faa
dodam24 <dodam0724@gmail.com>
2023-12-16 22:35:57 +0900

    .

--- kurly_server/src/main/java/com/example/market/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.market.repository;
-
-import com.example.market.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
